046e7e3c166fa38220c8100e514ec2b679a6a871
gfrisch <gfrisch@hotmail.fr>
2016-05-06 21:32:03 +0200

    Bean Questionnaire

---
+++ sr03/src/com/sr03/beans/Questionnaire.java
@@ -0,0 +1,40 @@
+package com.sr03.beans;
+
+import java.sql.Timestamp;
+
+public class Questionnaire {
+
+    private long   id;
+    private String    sujet;
+    private Boolean   status;
+    
+	public Questionnaire(){}
+	
+	public Questionnaire(long id, String sujet, Boolean status) {
+		this.id = id;
+		this.sujet = sujet;
+		this.status = status;
+	}
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId( long id ) {
+        this.id = id;
+    }
+
+    public void setSujet( String sujet ) {
+        this.sujet = sujet;
+    }
+    public String getSujet() {
+        return sujet;
+    }
+    
+    public Boolean getStatus() {
+        return status;
+    }
+    public void setStatus( Boolean status ) {
+        this.status = status;
+    }
+}
